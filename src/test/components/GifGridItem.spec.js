5d030172fd1e3710e95ad555a1f1848eb02b43a0
faog <fabiola.orellana.g@gmail.com>
2021-04-14 17:57:56 -0400

    test: GifGridItem component unit test

--- src/test/components/GifGridItem.spec.js
+++ src/test/components/GifGridItem.spec.js
@@ -5,11 +5,32 @@ import { GifGridItem } from '../../components/GifGridItem';
 describe('<GifGridItem /> Component', () => {
   let wrapper;
 
+  const props = {
+    title: 'Un titulo',
+    url: 'https://localhost/algo.jpg'
+  };
+
   beforeEach(() => {
-    wrapper = shallow(<GifGridItem />);
+    wrapper = shallow(<GifGridItem {...props} />);
   });
 
   test('should be render <GifGridItem />', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  test('should be render a paragraph with title', () => {
+    const p = wrapper.find('p');
+    expect(p.text().trim()).toBe(props.title);
+  });
+
+  test('should be have image equal to props', () => {
+    const img = wrapper.find('img');
+    expect(img.prop('src')).toBe(props.url);
+    expect(img.prop('alt')).toBe(props.title);
+  });
+
+  test('should be have card class', () => {
+    const div = wrapper.find('div');
+    expect(div.hasClass('card')).toBe(true);
   });
 });
